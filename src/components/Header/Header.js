8cdeea15d0ff6db9e90cf0510feec8b149339cfb
AsifKarim7 <asifkarim275@gmail.com>
2022-10-11 18:27:43 +0600

    header section done

--- src/components/Header/Header.js
+++ src/components/Header/Header.js
@@ -1,9 +1,17 @@
 import React from 'react';
 
 const Header = () => {
-    return (
-        <div>
 
+    return (
+        <div className='md:flex justify-center items-center my-10 gap-24'>
+            <div>
+                <h1 className='text-5xl font-bold text-yellow-600'>GREAT THINGS NEVER <br /> COME FROM <br /> COMFORT ZONE</h1>
+                <p className='md:mt-14 my-6 font-semibold md:text-xl'>Seek discomfrort. Face the harder challenge everyday.<br />So, try our quiz & enhance your knowledge skills.</p>
+            </div>
+
+            <div className='flex justify-center'>
+                <img className='md:w-full w-11/12' src='https://i.ibb.co/kVZRVgP/Quiz-Mania.jpg' alt='quiz-mania'></img>
+            </div>
         </div>
     );
 };
--- src/components/Home/Home.js
+++ src/components/Home/Home.js
@@ -7,17 +7,19 @@ const Home = () => {
     const quizTopics = useLoaderData();
 
     return (
-        <div className='home-container flex justify-center'>
+        <div className='home-container'>
             <div className='header-container'>
                 <Header></Header>
             </div>
-            <div className='grid grid-cols-2 gap-28 w-1/2 m-20'>
-                {
-                    quizTopics.data.map(quizTopic => <QuizTopic
-                        key={quizTopic.id}
-                        quizTopic={quizTopic}
-                    ></QuizTopic>)
-                }
+            <div className='flex justify-center'>
+                <div className='grid grid-cols-2 gap-28 w-1/2 my-14'>
+                    {
+                        quizTopics.data.map(quizTopic => <QuizTopic
+                            key={quizTopic.id}
+                            quizTopic={quizTopic}
+                        ></QuizTopic>)
+                    }
+                </div>
             </div>
         </div>
     );
