be3569ea76918b84fe2babe9eed2265b5b35bfb3
AsifKarim7 <asifkarim275@gmail.com>
2022-10-11 22:12:44 +0600

    Quiz toasts added

--- src/components/Option/Option.js
+++ src/components/Option/Option.js
@@ -1,10 +1,40 @@
 import React from 'react';
+import { ToastContainer, toast } from 'react-toastify';
+import 'react-toastify/dist/ReactToastify.css';
+const Option = ({ option, correctAnswer, id }) => {
 
-const Option = ({ option, handleRadioButton }) => {
+    const handleRadioButton = (value) => {
+        if (value === correctAnswer) {
+            toast.success('Correct Answer!', {
+                position: "top-center",
+                autoClose: 2500,
+                hideProgressBar: false,
+                closeOnClick: true,
+                pauseOnHover: true,
+                draggable: false,
+                progress: undefined,
+                theme: "colored",
+            });
+        }
+        else {
+            toast.error('Wrong Answer!', {
+                position: "top-center",
+                autoClose: 2500,
+                hideProgressBar: false,
+                closeOnClick: true,
+                pauseOnHover: true,
+                draggable: false,
+                progress: undefined,
+                theme: "colored",
+            });
+        }
+    }
+
     return (
         <div>
+            <ToastContainer></ToastContainer>
             <label>
-                <input type="radio" name="options" onClick={() => handleRadioButton(option)} />
+                <input className='cursor-pointer' type="radio" name={id} onClick={() => handleRadioButton(option)} />
                 {option}
             </label>
         </div>
--- src/components/Question/Question.js
+++ src/components/Question/Question.js
@@ -1,24 +1,32 @@
 import React from 'react';
 import Option from '../Option/Option';
+import { EyeIcon } from '@heroicons/react/24/solid'
+import { toast, ToastContainer } from 'react-toastify';
+
 
 const Question = ({ quizQuestion }) => {
-    const { options, question, correctAnswer } = quizQuestion;
-    console.log(quizQuestion)
+    const { options, question, correctAnswer, id } = quizQuestion;
 
-    const handleRadioButton = (value) => {
-        if (value === correctAnswer) {
-            alert('congo')
-        }
-        else {
-            alert('wrong answer')
-        }
+    const handleIcon = (correctAnswer) => {
+        toast.success(correctAnswer, {
+            position: "top-center",
+            autoClose: 2500,
+            hideProgressBar: false,
+            closeOnClick: true,
+            pauseOnHover: true,
+            draggable: false,
+            progress: undefined,
+            theme: "colored",
+        });
     }
 
     return (
         <div>
             <div className='border-2'>
-                <div className='py-6'>
-                    <h1>Question: {question}</h1>
+                <div className='py-6 flex justify-center'>
+                    <h1 className='mr-24'>Question: {question}</h1>
+                    <EyeIcon onClick={() => { handleIcon(correctAnswer) }} className="h-6 w-6 text-blue-500 cursor-pointer"></EyeIcon>
+                    <ToastContainer></ToastContainer>
                 </div>
 
                 <div>
@@ -26,7 +34,8 @@ const Question = ({ quizQuestion }) => {
                         options.map((option, idx) => <Option
                             key={idx}
                             option={option}
-                            handleRadioButton={handleRadioButton}
+                            correctAnswer={correctAnswer}
+                            id={id}
                         ></Option>)
                     }
                 </div>
